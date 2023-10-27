380aae58dde6cb3ff41612a5b4a2221b63c49528
disorderc <147068267+disorderc@users.noreply.github.com>
2023-10-27 11:05:13 +0200

    winScreen.js

--- js/winScreen.js
+++ js/winScreen.js
@@ -15,6 +15,7 @@ class WinScreen {
     init() {
         this.backgroundElement = document.createElement("figure")
         this.backgroundElement.style.backgroundImage = ("url('./img/5.png')")
+        this.backgroundElement.style.zIndex = "2"
         this.backgroundElement.style.position = "absolute"
         this.backgroundElement.style.width = `${this.backgroundSize.w}px`
         this.backgroundElement.style.height = `${this.backgroundSize.h}px`
@@ -23,6 +24,8 @@ class WinScreen {
 
         this.octopus = document.createElement("figure")
         this.octopus.style.backgroundImage = ("url('./img/win.png')")
+        this.octopus.style.zIndex = "2"
+
         this.octopus.style.position = "absolute"
         this.octopus.style.width = "400px"
         this.octopus.style.height = "600px"
@@ -35,6 +38,7 @@ class WinScreen {
 
         this.win = document.createElement("figure")
         this.win.style.backgroundImage = ("url('./img/winText.png')")
+        this.win.style.zIndex = "2"
         this.win.style.position = "absolute"
         this.win.style.width = "200px"
         this.win.style.height = "200px"
